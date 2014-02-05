e2f2e0f280ff55349dd307d5e312782b386c1903
ezxile <randomey@gmail.com>
2014-02-05 16:09:38 -0700

    Update scale.fix.js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,17 +1,40 @@
-var metas = document.getElementsByTagName('meta');
-var i;
-if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-    }
-  }
-  document.addEventListener("gesturestart", gestureStart, false);
-}
-function gestureStart() {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-    }
-  }
-}
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+	
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<title>ezxile - GEOE485L</title>
+		<style type=text/css>
+			
+		</style>
+	</head>
+	<body>
+		<h1>My Geography 485L Home Page</h1>
+		<p>About me</p>
+		<p>Why I'm taking this course</p>
+		  <li> 
+		
+		<h2>Topics I'm interested in</h2>
+		<ul>
+			<li>New Mexico landscapes</li>
+			<li>GIS</li>
+			<li>Graphic Design</li>
+			<li>Urban Geography</li>
+		</ul>
+		
+		<h2>Types of data I have worked with (themes & formats)</h2>
+		<ul>
+			<li>shapefiles</li>
+			<li>.kml files</li>
+			<li>.xml files</li>
+			<li>Adove InDesign files</li>
+		</ul>
+		
+		<h2>Milestones</h2>
+		<p><a href="">Milestone 2</a></p>
+		
+		<h2>Deep Dives</h2>
+		<p><a href="">Deep Dive 1</a></p>
+	</body>
+</html>
